d644b1f591458ca4167264fdd3ef569201ae9250
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2013-04-26 20:32:46 -0500

    Initial add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superheroes::Application.config.secret_token = '72527972a3aaaac70a6c36e5aa72d303bf4ef784d6e50946955f5265d3b21d4562db218846589eb30e8bffe7c0bf5cfc9b26eb301b02ab24fd0b31970e2921c7'
